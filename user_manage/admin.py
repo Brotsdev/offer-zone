02851460de46e0040a95f45556523c58a8a92767
Vishnu VP <vishnuvvp007@gmail.com>
2023-04-22 10:23:38 +0530

    admin

--- user_manage/admin.py
+++ user_manage/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+class UserManageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'username','created','is_active')
+admin.site.register(LoginUser, UserManageAdmin)
